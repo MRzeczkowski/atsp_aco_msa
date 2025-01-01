cf3e45f0e3829734947d122384beac46021b28e0
MRzeczkowski <mateusz.rzeczkowski96@gmail.com>
2025-01-01 16:41:44 +0100

    minor refactor and changes to plotting

--- modules/utilities/utilities.go
+++ modules/utilities/utilities.go
@@ -4,7 +4,83 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/font"
+	"gonum.org/v1/plot/palette/moreland"
+	"gonum.org/v1/plot/plotter"
 )
+
+type HeatMapData struct {
+	data       [][]float64
+	cellWidth  float64
+	cellHeight float64
+	minX       float64
+	minY       float64
+}
+
+func (h *HeatMapData) Dims() (c, r int) {
+	return len(h.data[0]), len(h.data)
+}
+
+func (h *HeatMapData) X(c int) float64 {
+	return h.minX + float64(c)*h.cellWidth
+}
+
+func (h *HeatMapData) Y(r int) float64 {
+	// Reverse Y-axis to make the origin top-left
+	return h.minY + float64(len(h.data)-1-r)*h.cellHeight
+}
+
+func (h *HeatMapData) Z(c, r int) float64 {
+	return h.data[r][c]
+}
+
+var (
+	plotWidth  font.Length = 800
+	plotLength font.Length = 800
+)
+
+func SaveHeatmapFromMatrix(matrix [][]float64, plotTitle, plotPath string) error {
+	heatmapData := &HeatMapData{
+		data:       matrix,
+		cellWidth:  1.0,
+		cellHeight: 1.0,
+		minX:       0.0,
+		minY:       0.0,
+	}
+
+	heatmapPlot := plot.New()
+	heatmapPlot.Title.Text = plotTitle
+	heatmapPlot.HideAxes()
+
+	palette := moreland.SmoothBlueRed().Palette(255)
+	heatmap := plotter.NewHeatMap(heatmapData, palette)
+
+	heatmapPlot.Add(heatmap)
+
+	return heatmapPlot.Save(plotWidth, plotLength, plotPath)
+}
+
+func SaveHistogramFromData(data []float64, bins int, plotTitle, plotPath string) error {
+	values := make(plotter.Values, len(data))
+	copy(values, data)
+
+	histogramPlot := plot.New()
+	histogramPlot.Title.Text = plotTitle
+	histogramPlot.X.Label.Text = "Value"
+	histogramPlot.X.Min = 1.0
+	histogramPlot.Y.Label.Text = "Frequency"
+
+	hist, err := plotter.NewHist(values, bins)
+	if err != nil {
+		return err
+	}
+
+	histogramPlot.Add(hist)
+
+	return histogramPlot.Save(plotWidth, plotLength, plotPath)
+}
 
 func ExtractNumber(input string) (int, error) {
 
